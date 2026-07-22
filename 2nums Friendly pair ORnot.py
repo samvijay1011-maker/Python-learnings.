0d0a1d9bb23e0bd35491ed94ab40f8c168e96ba4
samvijay1011-maker <samvijay1011@gmail.com>
2026-07-22 21:04:53 +0530

    Add script to check for friendly pairs of numbers

---
+++ 2nums Friendly pair ORnot.py
@@ -0,0 +1,16 @@
+def printDivisors(n, factors):
+    i = 1
+    while i <= n:
+        if n % i == 0:
+            factors.append(i)
+        i = i + 1
+    return sum(factors) - n
+
+
+if __name__ == "__main__":
+    number1, number2 = 6, 28
+
+    if printDivisors(number1, []) / number1 == printDivisors(number2, []) / number2:
+        print("Friendly pair")
+    else:
+        print("Not a Friendly Pair")
